a6a63d5cb43a65c2ad7d236b5febc57b99be1126
Carsen Berger <carsen.berger@gmail.com>
2022-03-21 15:18:22 -0500

    Use test fixtures in unit tests

--- tests/unit/test_fitter.py
+++ tests/unit/test_fitter.py
@@ -3,29 +3,26 @@ import pytest
 
 from volfitter.domain.datamodel import (
     Option,
-    OptionKind,
-    ExerciseStyle,
     RawIVCurve,
     RawIVPoint,
 )
 from volfitter.domain.fitter import PassThroughSurfaceFitter
 
-_EXPIRY = dt.datetime(2020, 2, 3, 15, 0)
 
-
-def test_pass_through_fitter_returns_midpoint_vol_per_strike():
-    option_1 = _create_option(100, OptionKind.CALL)
-    option_2 = _create_option(100, OptionKind.PUT)
-    option_3 = _create_option(200, OptionKind.CALL)
-    option_4 = _create_option(200, OptionKind.PUT)
-
+def test_pass_through_fitter_returns_midpoint_vol_per_strike(
+    jan_expiry: dt.datetime,
+    jan_100_call: Option,
+    jan_100_put: Option,
+    jan_110_call: Option,
+    jan_110_put: Option,
+):
     raw_iv_curve = RawIVCurve(
-        _EXPIRY,
+        jan_expiry,
         {
-            option_1: RawIVPoint(option_1, 1.1, 1.9),
-            option_2: RawIVPoint(option_2, 1.0, 2.0),
-            option_3: RawIVPoint(option_3, 1.1, 1.9),
-            option_4: RawIVPoint(option_4, 1.2, 2.2),
+            jan_100_call: RawIVPoint(jan_100_call, 1.1, 1.9),
+            jan_100_put: RawIVPoint(jan_100_put, 1.0, 2.0),
+            jan_110_call: RawIVPoint(jan_110_call, 1.1, 1.9),
+            jan_110_put: RawIVPoint(jan_110_put, 1.2, 2.2),
         },
     )
     victim = PassThroughSurfaceFitter()
@@ -33,8 +30,4 @@ def test_pass_through_fitter_returns_midpoint_vol_per_strike():
     final_iv_curve = victim._fit_curve_model(raw_iv_curve)
 
     assert pytest.approx(final_iv_curve.points[100].vol) == (1.1 + 1.9) / 2
-    assert pytest.approx(final_iv_curve.points[200].vol) == (1.2 + 1.9) / 2
-
-
-def _create_option(strike: float, option_kind: OptionKind) -> Option:
-    return Option("AMZN", _EXPIRY, strike, option_kind, ExerciseStyle.AMERICAN, 100)
+    assert pytest.approx(final_iv_curve.points[110].vol) == (1.2 + 1.9) / 2
--- tests/unit/test_service.py
+++ tests/unit/test_service.py
@@ -2,6 +2,8 @@ import datetime as dt
 
 from unittest.mock import Mock
 
+import pytest
+
 from volfitter.adapters.current_time_supplier import AbstractCurrentTimeSupplier
 from volfitter.adapters.final_iv_consumer import AbstractFinalIVConsumer
 from volfitter.adapters.raw_iv_supplier import AbstractRawIVSupplier
@@ -10,16 +12,51 @@ from volfitter.domain.fitter import AbstractSurfaceFitter
 from volfitter.service_layer.service import VolfitterService
 
 
-def test_volfitter_service_passes_raw_surface_through_fitter_to_consumer():
-    current_time = dt.datetime(2022, 10, 31, 1, 23)
-    raw_iv_surface = RawIVSurface(current_time, {})
-    final_iv_surface = FinalIVSurface(current_time, {})
+@pytest.fixture
+def raw_iv_surface(current_time: dt.datetime) -> RawIVSurface:
+    return RawIVSurface(current_time, {})
 
-    current_time_supplier = _create_current_time_supplier(current_time)
-    raw_iv_supplier = _create_raw_iv_supplier(raw_iv_surface)
-    surface_fitter = _create_surface_fitter(final_iv_surface)
-    final_iv_consumer = _create_final_iv_consumer()
 
+@pytest.fixture
+def final_iv_surface(current_time: dt.datetime) -> FinalIVSurface:
+    return FinalIVSurface(current_time, {})
+
+
+@pytest.fixture
+def current_time_supplier(current_time: dt.datetime) -> Mock:
+    current_time_supplier = Mock(spec_set=AbstractCurrentTimeSupplier)
+    current_time_supplier.get_current_time.return_value = current_time
+    return current_time_supplier
+
+
+@pytest.fixture
+def raw_iv_supplier(raw_iv_surface: RawIVSurface) -> Mock:
+    raw_iv_supplier = Mock(spec_set=AbstractRawIVSupplier)
+    raw_iv_supplier.get_raw_iv_surface.return_value = raw_iv_surface
+    return raw_iv_supplier
+
+
+@pytest.fixture
+def surface_fitter(final_iv_surface: FinalIVSurface) -> Mock:
+    surface_fitter = Mock(spec_set=AbstractSurfaceFitter)
+    surface_fitter.fit_surface_model.return_value = final_iv_surface
+    return surface_fitter
+
+
+@pytest.fixture
+def final_iv_consumer() -> Mock:
+    return Mock(spec_set=AbstractFinalIVConsumer)
+
+
+def test_volfitter_service_passes_raw_surface_through_fitter_to_consumer(
+    current_time: dt.datetime,
+    raw_iv_surface: RawIVSurface,
+    final_iv_surface: FinalIVSurface,
+    current_time_supplier: AbstractCurrentTimeSupplier,
+    raw_iv_supplier: Mock,
+    surface_fitter: Mock,
+    final_iv_consumer: Mock,
+):
     victim = VolfitterService(
         current_time_supplier, raw_iv_supplier, surface_fitter, final_iv_consumer
     )
@@ -29,25 +66,3 @@ def test_volfitter_service_passes_raw_surface_through_fitter_to_consumer():
     raw_iv_supplier.get_raw_iv_surface.assert_called_once_with(current_time)
     surface_fitter.fit_surface_model.assert_called_once_with(raw_iv_surface)
     final_iv_consumer.consume_final_iv_surface.assert_called_once_with(final_iv_surface)
-
-
-def _create_current_time_supplier(current_time: dt.datetime) -> Mock:
-    current_time_supplier = Mock(spec_set=AbstractCurrentTimeSupplier)
-    current_time_supplier.get_current_time.return_value = current_time
-    return current_time_supplier
-
-
-def _create_raw_iv_supplier(raw_iv_surface: RawIVSurface) -> Mock:
-    raw_iv_supplier = Mock(spec_set=AbstractRawIVSupplier)
-    raw_iv_supplier.get_raw_iv_surface.return_value = raw_iv_surface
-    return raw_iv_supplier
-
-
-def _create_surface_fitter(final_iv_surface: FinalIVSurface) -> Mock:
-    surface_fitter = Mock(spec_set=AbstractSurfaceFitter)
-    surface_fitter.fit_surface_model.return_value = final_iv_surface
-    return surface_fitter
-
-
-def _create_final_iv_consumer() -> Mock:
-    return Mock(spec_set=AbstractFinalIVConsumer)
